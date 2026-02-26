15734dcc10b83653a03fcb8736df5c5c3bc23930
Kento <rogeraabbccdd@gmail.com>
2026-02-26 16:26:08 +0800

    Add teammate check

--- Knife1HitKill.cs
+++ Knife1HitKill.cs
@@ -1,8 +1,9 @@
-using CounterStrikeSharp.API;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Cvars;
 
 namespace Knife1HitKill;
 
@@ -12,6 +13,7 @@ public class Knife1HitKill : BasePlugin
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "Kento";
     public override string ModuleDescription => "Knife 1 Hit Kill";
+    private ConVar? mp_teammates_are_enemies = ConVar.Find("mp_teammates_are_enemies");
 
     public override void Load(bool hotReload)
     {
@@ -47,6 +49,15 @@ public class Knife1HitKill : BasePlugin
         ) return HookResult.Continue;
         
         CCSPlayerPawn pawn = entity.As<CCSPlayerPawn>();
+
+        if (mp_teammates_are_enemies != null)
+        {
+            bool ffa = mp_teammates_are_enemies.GetPrimitiveValue<bool>();
+            if (!ffa && pawn.TeamNum == info.Attacker.Value.TeamNum)
+            {
+                return HookResult.Continue;
+            }
+        }
 
         info.Damage = pawn.Health + pawn.ArmorValue;
 
